a9f0219f571bab0433d2c74b49a376be9f98cc22
supercool27 <manojpali@live.com>
2022-06-13 18:16:39 +0530

    yoursafty concern

---
+++ App.js
@@ -0,0 +1,96 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useEffect,useState} from 'react';
+import { PermissionsAndroid } from 'react-native';
+import CallLogs from 'react-native-call-log';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App =() => {
+  const [listData, setListDate] = useState([]);
+  useEffect(() => {
+    CallLogFunc();
+ });
+
+  const CallLogFunc = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_CALL_LOG,
+        {
+          title: " Permission neeeded",
+          message: "Allow App to access your phone call logs",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK",
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("CALL LOG permission granted");
+        CallLogs.loadAll().then((c) => setListDate(c));
+        CallLogs.load(300).then((c) => console.log(c));
+      } else {
+        console.log("CALL LOG permission denied");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+   
+      <ScrollView>
+       
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
